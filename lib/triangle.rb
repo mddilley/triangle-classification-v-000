659ae0e7f376492d322a5ce72181706cf17c4d2e
Mike Dilley <mike.dilley@gmail.com>
2018-10-29 17:39:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
 
   def kind
     #binding.pry
-    if !@sides.include?(1..(Infinity))
+    if !@sides.include?(1..Float::INFINITY)
       raise TriangleError
     else
       triangle_type
